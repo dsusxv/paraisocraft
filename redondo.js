f722112f78f00f22a0c0dcd2a5faea85b94e8db6
dsusxv <92190747+dsusxv@users.noreply.github.com>
2021-12-29 02:22:09 -0300

    Add files via upload

---
+++ redondo.js
@@ -0,0 +1,6 @@
+function myfunction(){
+   let x =prompt('CODE:')
+   if(x === '1-69420DJDlkskqosjkAHksjjbAJK 2-69420AjwqQqe 3-21Wjirdfreoeiew 4-23DFGjiDklV'){
+      alert('FELICIDADES POR PERDER TU TIEMPO!')
+   }
+}
