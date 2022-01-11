5e9e04d67e45d3793860f3385381fadb30acc582
dsusxv <92190747+dsusxv@users.noreply.github.com>
2022-01-10 23:20:41 -0300

    Update redondo.js

--- redondo.js
+++ redondo.js
@@ -1,7 +1,14 @@
 function myfunction(){
    let x =prompt('CODE:')
    if(x === '1-69420DJDlkskqosjkAHksjjbAJK 2-69420AjwqQqe 3-21Wjirdfreoeiew 4-23DFGjiDklV'){
-      alert('FELICIDADES POR PERDER TU TIEMPO!')
+      
+    var img = document.createElement("img");
+    img.src = "https://cdn.discordapp.com/attachments/859487662893170739/930246745009561640/unknown.png";
+
+    var div = document.getElementById("x");
+    div.appendChild(img);
+    block.setAttribute("style", "width:100%;height:100%;");
+      
    }else{
       alert('01000101 01001100 00100000 01000011 01001111 01000100 01000101 00100000 01000100 01000101 01000010 01000101 00100000 01001001 01010010 00100000 01010011 01000101 01010000 01000001 01010010 01000001 01000100 01001111')
    }
